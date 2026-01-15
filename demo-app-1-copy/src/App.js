6278583b46005788b05134dcf7fec5adf8069f54
swathykt <swathykt97@gmail.com>
2026-01-15 09:43:20 +0530

    Add files via upload
    Initial commit

---
+++ demo-app-1-copy/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+<div className="container mt-5">
+  <h1 className='text-primary text-center'>Welcome to My App</h1>
+
+  <button className="btn btn-success mt-3">Click Me</button>
+
+  <div className='alert alert-warning mt-4'>
+    This is a Bootstrap alert!
+
+  </div>
+
+</div>
+  );
+}
+
+export default App;
